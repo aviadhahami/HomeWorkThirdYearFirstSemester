3441f03a7a0d126ab8e2ab937c9783c23b7dcf2a
aviadhahami <aviadhahami@gmail.com>
2015-11-05 12:14:37 +0200

    added fb icon

--- Design Patterns/Ex01/FacebookIntegrationApp/FacebookIntegrationApp/App.xaml.cs
+++ Design Patterns/Ex01/FacebookIntegrationApp/FacebookIntegrationApp/App.xaml.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace FacebookIntegrationApp
