b7eaf468c7874d5c01bb3bdc9678c03009a81991
aviadhahami <aviadhahami@gmail.com>
2015-11-11 18:12:42 +0200

    Moved to another sub class

--- Design Patterns/Ex01/FacebookIntegrationApp/FacebookIntegrationApp/FBHandler.cs
+++ Design Patterns/Ex01/FacebookIntegrationApp/FacebookIntegrationApp/FBHandler.cs
@@ -15,6 +15,7 @@ namespace FacebookIntegrationApp
         private static string[] s_WeekDayStringArray = { "Sunday", "Monday", "Tuesday", "Wenesday", "Thursday", "Friday", "Saturday" };
         private static string[] s_MonthNameStringArray = { "January", "February", "March", "April", "May", "June", 
                                                             "July","August","September","October","November","December"};
+
         public static string PostStatistics(FacebookObjectCollection<Status> i_Statuses)
         {
             // Make sure we initiate this procedure only once
@@ -33,35 +34,18 @@ namespace FacebookIntegrationApp
                     months[time.Month] += status.LikedBy.Count;
                 }
 
-                m_preferredDay = s_WeekDayStringArray[findIndexOfMaxValueInArray(days)];
-                m_preferredMonth = s_MonthNameStringArray[findIndexOfMaxValueInArray(months)];
-                m_preferredHour = hoursToAmPm(findIndexOfMaxValueInArray(hours));
+                m_preferredDay = s_WeekDayStringArray[Utilities.FindIndexOfMaxValueInArray(days)];
+                m_preferredMonth = s_MonthNameStringArray[Utilities.FindIndexOfMaxValueInArray(months)];
+                m_preferredHour = Utilities.HoursToAmPm(Utilities.FindIndexOfMaxValueInArray(hours));
             }
+
+            // Generate final string
             string finalStr = "According to your statuses statistics we found that:" + Environment.NewLine +
                     "Usualy on " + m_preferredDay + "s you get the most likes." + Environment.NewLine +
                     "Time-wise, " + m_preferredHour + " is the best time" + Environment.NewLine +
                     "and " + m_preferredMonth + " is the best month.";
             return finalStr;
         }
-        // Convert hour string in 24 hrs format to 12 hrs format
-        private static string hoursToAmPm(int hour)
-        {
-            return DateTime.ParseExact(hour.ToString(), "HH", CultureInfo.CurrentCulture).ToString("hh:mm tt");
-        }
 
-        private static int findIndexOfMaxValueInArray(int[] days)
-        {
-            int maxIndex = 0;
-            int maxValue = 0;
-            for (int i = 0; i < days.Length; i++)
-            {
-                if (maxValue < days[i])
-                {
-                    maxValue = days[i];
-                    maxIndex = i;
-                }
-            }
-            return maxIndex;
-        }
     }
 }
