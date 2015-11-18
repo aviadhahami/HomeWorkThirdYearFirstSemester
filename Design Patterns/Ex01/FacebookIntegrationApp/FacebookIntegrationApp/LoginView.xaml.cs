a0e67d358afc5771e47a844cedf405f5637ca30e
aviadh <aviadhahami@gmail.com>
2015-11-18 20:08:44 +0200

    Added diagram

--- Design Patterns/Ex01/FacebookIntegrationApp/FacebookIntegrationApp/LoginView.xaml.cs
+++ Design Patterns/Ex01/FacebookIntegrationApp/FacebookIntegrationApp/LoginView.xaml.cs
@@ -15,40 +15,38 @@ using System.Windows.Shapes;
 
 namespace FacebookIntegrationApp
 {
-    /// <summary>
-    /// Interaction logic for MainView.xaml
-    /// </summary>
-    public partial class LoginView : Window
-    {
-        public LoginView()
-        {
-            InitializeComponent();
-        }
-        private void Login(object sender, RoutedEventArgs e)
-        {
-            string appId = "1056989264331616";
-            LoginResult result = FacebookWrapper.FacebookService.Login(appId,
-                "user_about_me",
-                "user_friends",
-                "publish_actions",
-                "user_events",
-                "user_posts",
-                "user_photos",
-                "user_status");
-            if (!string.IsNullOrEmpty(result.AccessToken))
-            {
+	/// <summary>
+	/// Interaction logic for MainView.xaml
+	/// </summary>
+	public partial class LoginView : Window
+	{
+		
+		public LoginView ()
+		{
+			InitializeComponent ();
+		}
 
-                // If we're good with the data, we pass it to the next view
-                User LoggedInUser = result.LoggedInUser;
-                MainView mainView = new MainView(LoggedInUser);
-                mainView.Show();
-                this.Close();
-            }
-            else
-            {
-                MessageBox.Show(result.ErrorMessage);
-            }
-          
-        }
-    }
+		private void Login (object sender, RoutedEventArgs e)
+		{
+			string appId = "1056989264331616";
+			LoginResult result = FacebookWrapper.FacebookService.Login (appId,
+				                     "user_about_me",
+				                     "user_friends",
+				                     "publish_actions",
+				                     "user_events",
+				                     "user_posts",
+				                     "user_photos",
+				                     "user_status");
+			if (!string.IsNullOrEmpty (result.AccessToken)) {
+
+				// If we're good with the data, we pass it to the next view
+				User LoggedInUser = result.LoggedInUser;
+				MainView mainView = new MainView (LoggedInUser);
+				mainView.Show ();
+				this.Close ();
+			} else {
+				MessageBox.Show (result.ErrorMessage);
+			}
+		}
+	}
 }
