e37cb7be36c9e5044543a961fa92b1798fef79a3
aviadh <aviadhahami@gmail.com>
2015-12-12 17:29:42 +0200

    Generated server pakcage

---
+++ Computer Networks/Ex03/src/server/Main.java
@@ -0,0 +1,15 @@
+package server;
+
+/**
+ *
+ * @author aviadh
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int port = 9000;
+
+        Server s = new Server(port);
+        s.listen();
+    }
+}
