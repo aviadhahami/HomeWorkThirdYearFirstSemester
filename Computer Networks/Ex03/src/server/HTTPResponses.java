9c8e1c920b4f23a82124803bd7a278eb92cd0e81
aviadh <aviadhahami@gmail.com>
2015-12-13 16:55:23 +0200

    Now returning by content type as well

--- Computer Networks/Ex03/src/server/HTTPResponses.java
+++ Computer Networks/Ex03/src/server/HTTPResponses.java
@@ -17,8 +17,13 @@ import java.util.Map;
 
 public class HTTPResponses {
 
+	// Private members
 	private static final Map<Integer, String> HTTPcodesHash;
+
+	// Final static strings
 	private static final String NO_SUCH_CODE = "No such HTTP code";
+	private static final String NO_SUCH_CONTENT_TYPE = "No such content type";
+	private static final String CONTENT_TYPE = "content-type: ";
 
 	// Populate the hash
 	static {
@@ -32,8 +37,26 @@ public class HTTPResponses {
 	}
 
 	// Public getter
-	public static String getResponse(int code) {
+	public static String getResponseCodeHeaderByCode(int code) {
 		String res = HTTPcodesHash.get(code);
 		return res == null ? NO_SUCH_CODE : res;
+	}
+
+	
+	public static String getContentTypeHeaderByType(String cType) {
+		String res = NO_SUCH_CONTENT_TYPE;
+		switch (cType) {
+		case "html":
+		case "text":
+			res = CONTENT_TYPE + "text/html";
+			break;
+		case "image":
+			res = CONTENT_TYPE + "image";
+			break;
+		case "icon":
+			res = CONTENT_TYPE + "icon";
+			break;
+		}
+		return res;
 	}
 }
