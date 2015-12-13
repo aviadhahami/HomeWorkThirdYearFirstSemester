f3fce1d2527dfa5df18f60f1000706daa470b270
aviadh <aviadhahami@gmail.com>
2015-12-13 17:17:09 +0200

    Removed unrelevant code

--- Computer Networks/Ex03/src/server/Main.java
+++ Computer Networks/Ex03/src/server/Main.java
@@ -7,6 +7,9 @@ package server;
 public class Main {
 
     public static void main(String[] args) {
+    	
+    	// TODO: load config here and pass it to server
+    	
         int port = 9000;
 
         Server s = new Server(port);
