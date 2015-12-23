425bfda9def45f5d7b2deb7bec56beaefbcd9f5c
aviadh <aviadhahami@gmail.com>
2015-12-23 19:27:36 +0200

    On work

--- InformationSecurity/HMAC/src/encryptors/HMAC.java
+++ InformationSecurity/HMAC/src/encryptors/HMAC.java
@@ -1,5 +1,6 @@
 package encryptors;
 
+import java.sql.Blob;
 import java.util.Arrays;
 import java.util.Iterator;
 
@@ -23,32 +24,57 @@ public class HMAC {
 	private static String computeHmac(byte[] bs) {
 
 		byte[] key = FilesContentHolder.getKeyFileContent();
-		byte[] paddedKey = null;
+		byte[] paddedKey = new byte[BLOCKSIZE];
 
 		// keys longer than 64byte are shortened
 		// FIXME: convert ASCII bytes to actual bytes (0x39, 0x59, ...)
 
-		if(key.length > BLOCKSIZE){
+		if (key.length > BLOCKSIZE) {
 			byte[] hashedKey = SHA1.encode(key);
-			Arrays.fill(paddedKey, (byte)0x00);
+			Arrays.fill(paddedKey, (byte) 0x00);
 			for (int i = 0; i < hashedKey.length; i++) {
 				paddedKey[i] = hashedKey[i];
 			}
+			// Keys shorter than BLOCKSIZE are zero-padded from the right to fit
+			// the BLOCKSIZE
+		} else if (key.length < BLOCKSIZE) {
+			Arrays.fill(paddedKey, (byte) 0x00);
+			for (int i = 0; i < key.length; i++) {
+				paddedKey[i] = key[i];
+			}
+		} else if (key.length == BLOCKSIZE) {
+			paddedKey = key;
 		}
-		
-		// FIXME: paddedKey is 20 bytes, should be 64
 
-		// Keys shorter than BLOCKSIZE are zero-padded from the right to fit
-		// the BLOCKSIZE
-
+		byte[] opad = new byte[BLOCKSIZE];
+		byte[] ipad = new byte[BLOCKSIZE];
+		Arrays.fill(opad, (byte) 0x5c);
+		Arrays.fill(ipad, (byte) 0x36);
+		for (int i = 0; i < BLOCKSIZE; i++) {
+			opad[i] = (byte) (opad[i] ^ paddedKey[i]);
+			ipad[i] = (byte) (ipad[i] ^ paddedKey[i]);
+		}
+		byte[] message = FilesContentHolder.getInputFileContent();
 		// o_key_pad = [0x5c * blocksize] ⊕ key // Where blocksize is that of
 		// the underlying hash function
 		// i_key_pad = [0x36 * blocksize] ⊕ key // Where ⊕ is exclusive or (XOR)
+		byte[] context = new byte[ipad.length + message.length];
+		System.arraycopy(ipad, 0, context, 0, ipad.length);
+		System.arraycopy(message, 0, context, ipad.length, message.length);
+		byte[] firstSha1 = SHA1.encode(context);
 
+		byte[] secondSha = new byte[firstSha1.length + opad.length];
+		System.arraycopy(opad, 0, secondSha, 0, opad.length);
+		System.arraycopy(firstSha1, 0, secondSha, opad.length, firstSha1.length);
 		// return hash(o_key_pad ∥ hash(i_key_pad ∥ message)) // Where ∥ is
 		// concatenation
 		// end function
-		return new String(sha);
+		for (byte b : SHA1.encode(secondSha)) {
+			System.out.print(Integer.toHexString(b));
+			System.out.print("_");
+		}
+
+		return new String(SHA1.encode(secondSha));
 	}
 
 }
