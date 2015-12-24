8696ec0d454d8725d744a7ed1e804e38c4fa802a
aviadh <aviadhahami@gmail.com>
2015-12-24 15:27:37 +0200

    :(

--- InformationSecurity/HMAC/src/parsers/Utils.java
+++ InformationSecurity/HMAC/src/parsers/Utils.java
@@ -62,4 +62,7 @@ public class Utils {
 		}
 		return new String(hexChars);
 	}
+	public static final byte[] intToByteArray(int value) {
+		return new byte[] { (byte) (value >>> 24), (byte) (value >>> 16), (byte) (value >>> 8), (byte) value };
+	}
 }
