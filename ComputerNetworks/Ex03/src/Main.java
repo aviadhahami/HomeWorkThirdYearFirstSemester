7081196ecca20dd7d98690c12a86ff3f3074d969
Aviad Hahami <aviadhahami@gmail.com>
2016-01-12 14:50:14 +0200

    Removed unsused index.html things

--- ComputerNetworks/Ex03/src/Main.java
+++ ComputerNetworks/Ex03/src/Main.java
@@ -1,5 +1,6 @@
 
 
+import java.io.File;
 /**
  *
  * @author aviadh
@@ -9,7 +10,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		// Give option to run via terminals. #DevOps
-		String defaultConfigPath = "../config/config.ini";
+		String defaultConfigPath = System.getProperty("user.dir")+File.separator  +".."+ File.separator + "config" + File.separator + "config.ini";
 		String configPath = args.length > 0 ? args[0] : defaultConfigPath;
 		Server s = new Server(ServerConfigLoader.load(configPath));
 		s.listen();
--- ComputerNetworks/Ex04/www/public/scripts/index.js
+++ ComputerNetworks/Ex04/www/public/scripts/index.js
@@ -5,37 +5,4 @@
 
 $(document).ready(function () {
 
-
-    var displayDisclaimer = function () {
-        $('.disclaimer').css('display', 'block');
-        $('.real-content').css('display', 'none');
-    };
-    var displayPage = function(){
-        $('.disclaimer').css('display', 'none');
-        $('.real-content').css('display', 'block');
-    };
-
-    // Decide what to display
-    if (!sessionStorage.hasOwnProperty('firstRun') || !sessionStorage.firstRun) {
-        displayDisclaimer();
-    }
-
-    $('#bad').on('click', function () {
-        alert(':(');
-        sessionStorage.firstRun = true;
-        displayPage();
-
-    });
-
-    $('#good').on('click', function () {
-        alert('Thanks!');
-        sessionStorage.firstRun = true;
-        displayPage();
-    });
-    $('#ok').on('click', function () {
-        displayPage();
-        sessionStorage.firstRun = true;
-        displayPage();
-    })
-
 });
