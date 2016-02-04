aff07e99b07d6d05a71dd647e04e43987bda1da3
Aviad Hahami <aviadhahami@gmail.com>
2016-02-04 19:38:02 +0200

    checking for input entry

--- ComputerNetworks/Ex04/www/public/scripts/index.js
+++ ComputerNetworks/Ex04/www/public/scripts/index.js
@@ -2,5 +2,20 @@
 
 // JQ
 $(document).ready(function(){
+    var portScanInput = $('#portScan');
+    var robotsInput =$('#robots');
+    var domain = $('#domain');
+    portScanInput.prop('disabled',true);
+    robotsInput.prop('disabled',true);
+
+    domain.on('keyup',function(e){
+        if(e.currentTarget.value.length >0){
+            portScanInput.prop('disabled',false);
+            robotsInput.prop('disabled',false);
+        }else{
+            portScanInput.prop('disabled',true);
+            robotsInput.prop('disabled',true);
+        }
+    });
 
 });
